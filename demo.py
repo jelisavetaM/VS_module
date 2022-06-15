71badc7778324fccd022b2c981389aa2b9120239
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-15 13:39:41 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -54,7 +54,7 @@ def get_datamap(datamap_json_file):
     datamap = {}
     questions_label_text = []
     #datamap_json = json.load(datamap_json_file)
-	datamap_json = "https://github.com/jelisavetaM/VS_module/blob/main/datamap.json"
+    datamap_json = "https://github.com/jelisavetaM/VS_module/blob/main/datamap.json"
     for var in datamap_json["variables"]:
         q_title = var["label"]
         answers = {}
