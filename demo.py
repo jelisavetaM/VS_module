935c715f1f2dbd6f6bde50050f5c722f11fdaf78
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-16 08:27:57 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -235,8 +235,8 @@ def splitEngine(measures, splitScheme, levels):
 
 def inputEntered ():
     with header2:
-        st.markdown("Data generated for project: **" + st.session_state.text_key + "**.") 
-        st.markdown("If you want to change project, just re-enter the number in the inout below and press Enter.")
+        st.markdown("Data generated for project: **" + st.session_state.text_key + "**.\n"
+        "If you want to change project, just re-enter the number in the inout below and press Enter.")
         st.markdown("-------------------------------------------------------------------------------------------")
 		
 header1 = st.container()
