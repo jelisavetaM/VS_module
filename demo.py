3acf3689989a750f4dd0b15082c876cf8ddcbc2f
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-17 10:25:21 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -174,7 +174,7 @@ def get_measure_df(measure, level, split):
 #######################################################################################################################################################################
 def splitEngine(measures, splitScheme, levels):
     global shoppingMergedData
-        dfAll_dict = {}
+    dfAll_dict = {}
 	for no_levels, splits in splitScheme.items():
 		tables = []
 		tables_by_measure = {}
