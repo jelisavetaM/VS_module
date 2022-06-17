10411f979c2834bbca546d9dbe32b4cf89508f2a
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-17 13:50:24 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,6 +6,8 @@ from openpyxl import load_workbook, Workbook
 from urllib.request import urlopen
 import requests, json
 import xlsxwriter
+
+
 def style_table(v):
     if v < 4:
         return 'color:green;'
