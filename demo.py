7809907a95c4a189885d05ceebede7376fed899c
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-19 20:41:34 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -700,9 +700,9 @@ with dataset:
             with open(ZipfileDotZip, "rb") as f:
                 bytes = f.read()
                 b64 = base64.b64encode(bytes).decode()
-                href = f"<button style='background-color: #033b6e'><a href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
+                href = f"<a class='download' href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
                     Click last model weights\
-                </a></button>"
+                </a>"
                 st.sidebar.markdown(href, unsafe_allow_html=True)
                 st.download_button('Proba', ZipfileDotZip, file_name= 'Export_' + st.session_state.text_key + '_version_by_measure.zip')
                 
