40d8bb011521457bf360844ee8874dc451229200
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 11:51:14 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -93,7 +93,7 @@ def get_df_with_answer_labels(df,vars_arr):
         df_return = df[vars_arr]
     
     for col in df_return.columns:
-        labels = list(set(df_return[col].values.tolist()))
+        labels = list(set(df_return[col].tolist()))
         for lab in labels:
             if datamap[col]["answers"] and lab in datamap[col]["answers"]:
                 df_return[col] = df_return[col].replace(lab, datamap[col]["answers"][lab])
