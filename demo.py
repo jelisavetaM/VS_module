b1b3a08d12a8e732c255b39e51a88598d4ab5067
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 02:32:36 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -721,6 +721,7 @@ with dataset:
                             ws.freeze_panes = ws["A" + str(cell.row)]
                             col_temp = re.sub(r'[^a-zA-Z]', '', ws.dimensions.split(":")[1])
                             ws.auto_filter.ref = "A" + str(cell.row) + ":" + col_temp + str(cell.row)
+                            ws.delete_cols(0,0)
                 wb_level.save("final_by_level.xlsx")
             
     
