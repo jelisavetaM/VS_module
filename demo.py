292854c13c84b9924a982728aa84e6cb22bc6913
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 03:24:02 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -706,6 +706,10 @@ with dataset:
                 wb_measure = load_workbook("final_by_measure.xlsx")
                 wb_level = load_workbook("final_by_level.xlsx")
                 
+                header_format = workbook.add_format({
+                    'bold': True,
+                    'fg_color': '#ffcccc',
+                    'border': 1})    
                 
                 for sheet in wb_measure.worksheets:
                     ws = wb_measure[sheet.title]
@@ -717,7 +721,9 @@ with dataset:
                             ws.auto_filter.ref = "A" + str(cell.row) + ":" + col_temp + str(cell.row)
                     ws.column_dimensions['C'].width = 35
                     ws.column_dimensions['D'].width = 30
-                    ws.column_dimensions['A'].hidden= True
+                    ws.write(0, 2, 3, header_format)
+                    if sheet.title == "Splits levels_1":
+                        ws.column_dimensions['A'].hidden= True
                 wb_measure.save("final_by_measure.xlsx")
                 
                 for sheet in wb_level.worksheets:
@@ -730,7 +736,8 @@ with dataset:
                             ws.auto_filter.ref = "A" + str(cell.row) + ":" + col_temp + str(cell.row)
                     ws.column_dimensions['C'].width = 35
                     ws.column_dimensions['D'].width = 30
-                    
+                    if sheet.title == "Splits levels_1":
+                        ws.column_dimensions['A'].hidden= True
                 wb_level.save("final_by_level.xlsx")
                 
                 
