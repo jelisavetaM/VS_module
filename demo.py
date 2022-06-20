49c48d87db140662edc6a2330c947be42b35eb06
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 12:38:38 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -94,7 +94,6 @@ def get_df_with_answer_labels(df,vars_arr):
     
     for col in df_return.columns.drop_duplicates(keep ='first'):
         st.write(col)
-        st.stop()
         labels = list(df_return[col])
         for lab in labels:
             if datamap[col]["answers"] and lab in datamap[col]["answers"]:
