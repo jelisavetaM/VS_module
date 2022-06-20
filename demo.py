0fb77960c90a33838cede904f8bbb948c141c1a1
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 03:10:41 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -715,7 +715,8 @@ with dataset:
                             ws.freeze_panes = ws["A" + str(cell.row)]
                             col_temp = re.sub(r'[^a-zA-Z]', '', ws.dimensions.split(":")[1])
                             ws.auto_filter.ref = "A" + str(cell.row) + ":" + col_temp + str(cell.row)
-                    ws.column_dimensions['C:D'].width = 20
+                    ws.column_dimensions['C'].width = 20
+                    ws.column_dimensions['D'].width = 20
                 wb_measure.save("final_by_measure.xlsx")
                 
                 for sheet in wb_level.worksheets:
@@ -726,7 +727,9 @@ with dataset:
                             ws.freeze_panes = ws["A" + str(cell.row)]
                             col_temp = re.sub(r'[^a-zA-Z]', '', ws.dimensions.split(":")[1])
                             ws.auto_filter.ref = "A" + str(cell.row) + ":" + col_temp + str(cell.row)
-                    # ws.set_column('B:D', 30)
+                                        ws.column_dimensions['C'].width = 20
+                    ws.column_dimensions['C'].width = 20
+                    ws.column_dimensions['D'].width = 20
                 wb_level.save("final_by_level.xlsx")
                 
                 
