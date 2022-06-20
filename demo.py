3fc4d7a397f1675963d682bbcd1961fee5a0f720
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 11:47:53 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -93,8 +93,6 @@ def get_df_with_answer_labels(df,vars_arr):
         df_return = df[vars_arr]
     
     for col in df_return.columns:
-        st.write(list(set(df_return[col].tolist())))
-        st.stop()
         labels = list(set(df_return[col].tolist()))
         for lab in labels:
             if datamap[col]["answers"] and lab in datamap[col]["answers"]:
