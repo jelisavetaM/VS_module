afebdb6b4f9ee21c58540592571913bfd1bfde4b
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-06-20 12:32:28 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -94,6 +94,7 @@ def get_df_with_answer_labels(df,vars_arr):
     
     for col in df_return.columns:
         st.write(type(df_return.columns))
+        st.write(df_return.columns.drop_duplicates(keep ='first'))
         st.stop()
         labels = list(set(df_return[col].values))
         for lab in labels:
